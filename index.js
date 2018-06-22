ff4c6560c0a330d52c0493d220384b03925b61f9
attila <attila@hertzkern.com>
2018-06-22 16:20:42 +0200

    generalize data reading, add times

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ const fs = require('fs');
 const app = express();
 const port = 3001;
 
-let runOptions = {};
+let data = {};
+
+const runPreconditions = {
+    optionsRead: false,
+    timesRead: false,
+    serverRunnning: false
+};
 
 const run = function () {
-    console.log("Running with options", runOptions);
+    for (let key of Object.keys(runPreconditions)) {
+        const value = runPreconditions[key];
+        if (!value) {
+            console.log("Not yet ready:", key);
+            return;
+        }
+    }
+
+    console.log("Running with options", data.options);
+    console.log("Running with times", data.times);
     const audioOptions = { channels: 2, bitDepth: 16, sampleRate: 44100 };
 
     // Create Decoder and Speaker
-    const decoder = lame.Decoder();
-    let speaker = new Speaker(audioOptions);
+    //const decoder = lame.Decoder();
+    //let speaker = new Speaker(audioOptions);
 };
 
 // respond with "hello world" when a GET request is made to the homepage
@@ -20,13 +35,22 @@ app.get('/health', function (req, res) {
 });
 
 app.listen(port, function () {
-    console.log("Listening");
-    fs.readFile('options.json', function (err, data) {
+    console.log("Listening on port", port);
+    runPreconditions.serverRunnning = true;
+    run();
+});
+
+const readFile = function (fieldName) {
+    fs.readFile(fieldName + '.json', function (err, readData) {
         if (err) {
             console.error("Error", err);
             throw err;
         }
-        runOptions = JSON.parse(data);
+        data[fieldName] = JSON.parse(readData);
+        runPreconditions[fieldName + 'Read'] = true;
         run();
     });
-});
+};
+
+readFile('options');
+readFile('times');
