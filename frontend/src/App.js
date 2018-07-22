1b92067a2cda078df36d0161f77b65a8ee2a3d7d
Attila <attila@hertzkern.com>
2018-07-22 11:21:28 +0200

    NDJ-0011: add stop music button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
-import { backendGetIpAddressesRequestAction, backendPutSkipToNextSongRequestAction, backendGetCurrentSongRequestAction, backendPutSkipToNextAlbumRequestAction } from './redux/backendMiddleware';
+import { backendGetIpAddressesRequestAction, backendPutSkipToNextSongRequestAction, backendGetCurrentSongRequestAction, backendPutSkipToNextAlbumRequestAction, backendPutStopMusicRequestAction } from './redux/backendMiddleware';
 import PropTypes from 'prop-types';
 import ControlButton from './common/ControlButton';
 
@@ -41,6 +41,7 @@ class App extends Component {
     const buttons = [];
     buttons.push(<ControlButton label="Skip to next song" action={putSkipToNextSong(this.props.dispatch)} key="next-song" />);
     buttons.push(<ControlButton label="Skip to next album" action={putSkipToNextAlbum(this.props.dispatch)} key="next-album" />);
+    buttons.push(<ControlButton label="Stop music" action={putStopMusic(this.props.dispatch)} key="stop-music" />);
     return <div>{buttons}</div>
   }
 
@@ -68,6 +69,10 @@ const putSkipToNextSong = (dispatch) => () => {
 
 const putSkipToNextAlbum = (dispatch) => () => {
   dispatch(backendPutSkipToNextAlbumRequestAction);
+};
+
+const putStopMusic = (dispatch) => () => {
+  dispatch(backendPutStopMusicRequestAction);
 };
 
 let mapStateToPropsCounter = 0;
--- frontend/src/redux/backendMiddleware.js
+++ frontend/src/redux/backendMiddleware.js
@@ -2,12 +2,14 @@ const BACKEND_GET_IP_ADDRESSES_REQUEST = "BACKEND_GET_IP_ADDRESSES_REQUEST";
 const BACKEND_GET_CURRENT_SONG_REQUEST = "BACKEND_GET_CURRENT_SONG_REQUEST";
 const BACKEND_PUT_SKIP_TO_NEXT_SONG = "BACKEND_PUT_SKIP_TO_NEXT_SONG";
 const BACKEND_PUT_SKIP_TO_NEXT_ALBUM = "BACKEND_PUT_SKIP_TO_NEXT_ALBUM";
+const BACKEND_PUT_STOP_MUSIC = "BACKEND_PUT_STOP_MUSIC";
 
 const request = (key, payload, callback) => { return { type: key, payload: { payload: payload, callback: callback } }; };
 
 export const backendGetIpAddressesRequestAction = request(BACKEND_GET_IP_ADDRESSES_REQUEST);
 export const backendPutSkipToNextSongRequestAction = request(BACKEND_PUT_SKIP_TO_NEXT_SONG);
 export const backendPutSkipToNextAlbumRequestAction = request(BACKEND_PUT_SKIP_TO_NEXT_ALBUM);
+export const backendPutStopMusicRequestAction = request(BACKEND_PUT_STOP_MUSIC);
 export const backendGetCurrentSongRequestAction = request(BACKEND_GET_CURRENT_SONG_REQUEST);
 
 const baseUrl = "http://localhost:3001";
@@ -51,6 +53,9 @@ export const asyncDispatchMiddleware = store => next => action => {
             break;
         case BACKEND_PUT_SKIP_TO_NEXT_ALBUM:
             putAndDo("/api/skip-album", {}, logItAndReloadCurrentSong("PUT SKIP TO NEXT ALBUM SUCCESSFUL", store))
+            break;
+        case BACKEND_PUT_STOP_MUSIC:
+            putAndDo("/api/music-stop", {}, logItAndReloadCurrentSong("PUT STOP MUSIC SUCCESSFUL", store))
             break;
         case BACKEND_GET_CURRENT_SONG_REQUEST:
             fetchAndWriteTo("/api/current-song", "currentSong", action.type, store);
