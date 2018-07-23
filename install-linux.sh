7db098f0691423d6d3f80acbfdcd959fe1da19b8
Attila <attila@hertzkern.com>
2018-07-23 15:20:24 +0200

    add linux run script, add vim to linux install script

--- install-linux.sh
+++ install-linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 curl -sL https://deb.nodesource.com/setup_9.x -o nodesource_setup.sh
-sudo nodesource_setup.sh
+sudo bash nodesource_setup.sh
 rm nodesource_setup.sh
 sudo apt-get update
-sudo apt-get install -y nodejs libasound2-dev make gcc g++
+sudo apt-get install -y vim nodejs libasound2-dev make gcc g++
 npm install -g react-scripts
 npm install
 cd frontend && npm install
