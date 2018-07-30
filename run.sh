a6aaad98240545f7864e100fafa7c3fd6b58327d
Attila <attila@hertzkern.com>
2018-07-30 11:53:52 +0200

    tidy up run bash scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo $DATE > run.log
 pwd >> run.log
 git pull || true
 npm run start "/" "/media/eltern/MYLINUXLIVE/altincamp" >> run.log
--- run_local.sh
+++ run_local.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo $DATE > run.log
-pwd >> run.log
 git pull || true
 npm run start_dev "/" "/home/attila/Music/node-dj-long/"
