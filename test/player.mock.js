4e0ec3f88a879e5bb06aa0c37e6bee8aa820a3b1
attila <attila@hertzkern.com>
2018-08-02 13:39:40 +0300

    adapt player mock to new version of player

--- test/player.mock.js
+++ test/player.mock.js
@@ -1,53 +1,52 @@
 const tools = require('./tools');
 const fs = require('fs');
-let dj;
 
-const audioOptions = { channels: 2, bitDepth: 16, sampleRate: 44100 };
-const debug = true;
-const log = tools.logGenerator(() => debug, "player");
+const logLog = true;
+const logDebug = false;
+const log = tools.logGenerator(() => logLog, "player");
+const debug = tools.logGenerator(() => logDebug, "player");
+let inputStream;
+let isPlaying = false;
 
-class Player {
-    constructor(fileName) {
-        this.fileName = fileName;
-        this.inputStream = fs.createReadStream(this.fileName);
+const stopPlayback = function () {
+    if (isPlaying) {
+        log("Stopping playback of old song...");
+        inputStream.close();
+        //inputStream.unpipe(speaker).unpipe(decoder);
+        isPlaying = false;
     }
+};
 
-    play() {
-        log("Starting to play ", this.fileName);
-        // this.inputStream.pipe(decoder).pipe(speaker);
-        setTimeout(() => {
-            log("mock play issues next playNextSong()");
-            playNextSong();
-        }, 10000);
-    }
+const playSong = function (path, callbackWhenDone, dj) {
+    stopPlayback();
+    log("playSong called with", path);
+    inputStream = fs.createReadStream(path);
 
-    stop() {
-        log("Stopping to play ", this.fileName);
-        //  this.inputStream.unpipe(decoder).unpipe(speaker);
-        this.inputStream.close();
-    }
-}
-
-let currentPlayer;
-
-const playSong = function () {
-    log("playSong() called");
-    currentPlayer = new Player(dj.getCurrentSong().path);
-    currentPlayer.play();
-}
-const playNextSong = function () {
-    log("playNextSong() called");
-    dj.switchToNextSong();
+    log("Starting to play ", path);
+    isPlaying = true;
+    // this.inputStream.pipe(decoder).pipe(speaker);
+    setTimeout(() => {
+        log("mock play issues callbackWhenDone()");
+        isPlaying = false;
+        callbackWhenDone();
+    }, 10000);
 }
 module.exports = {
-    skipSong: function () {
-        currentPlayer.stop();
-        playSong();
-    },
-    start: function () {
-        playSong();
-    },
-    setDj: function (theDj) {
-        dj = theDj;
-    },
+    /**
+     * If existing, stops any running playbacks. Then starts the playback of the mp3 file with given path.
+     * @param {string} path The path to the mp3 file to be played.
+     * @param {function} callbackWhenDone The function to call when done playing.
+     * @param {object} dj The dj instance to use as a parameter when calling callbackWhenDone()
+     */
+    playSong: playSong,
+
+    /**
+     * If existing, stops any running playbacks. 
+     */
+    stopPlayback: stopPlayback,
+
+    /**
+     * @returns {boolean} whether there is currently a playback
+     */
+    isPlaying: () => isPlaying,
 };
