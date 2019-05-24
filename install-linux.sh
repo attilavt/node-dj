e03f71cb9449dd3d422fa60e4d28fcd95d230682
Attila <attila@hertzkern.com>
2019-05-24 13:25:27 +0300

    update node version in install script, prepare for generated run.sh

--- install-linux.sh
+++ install-linux.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-curl -sL https://deb.nodesource.com/setup_9.x -o nodesource_setup.sh
+set -e
+sudo apt-get install -y curl
+curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 rm nodesource_setup.sh
 sudo apt-get update
 sudo apt-get install -y vim nodejs libasound2-dev make gcc g++
-npm install -g react-scripts
+sudo npm install -g react-scripts
 npm install
 cd frontend && npm install
